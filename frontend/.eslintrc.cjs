b528ed00d270dda85e5200a1153bf75c8917c8ed
Victor Valenzuela <khancerberus@hotmail.cl>
2024-07-18 11:02:45 -0400

    chore: initialize front end project

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'jsx-quotes': ['error', 'prefer-double'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    'space-before-function-paren': 'off',
+    'multiline-ternary': ['error', 'always-multiline']
+  },
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App(): JSX.Element {
+
+  return (
+    <main>
+
+    </main>
+  )
+}
+
+export default App
