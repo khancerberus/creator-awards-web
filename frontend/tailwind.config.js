d7e50f997d1ffda15d4334d22816877b6785b666
Victor Valenzuela <khancerberus@hotmail.cl>
2024-07-27 17:41:23 -0400

    chore: Update primary and secondary color variables in CSS and Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,17 +4,20 @@ export default {
     theme: {
         extend: {
             colors: {
+                neutral: {
+                    900: 'var(--neutral-900)',
+                },
                 primary: {
                     50: 'var(--primary-50)',
-                    100: 'var(--primary-50)',
-                    200: 'var(--primary-50)',
-                    300: 'var(--primary-50)',
-                    400: 'var(--primary-50)',
-                    500: 'var(--primary-50)',
-                    600: 'var(--primary-50)',
-                    700: 'var(--primary-50)',
-                    800: 'var(--primary-50)',
-                    900: 'var(--primary-50)'
+                    100: 'var(--primary-100)',
+                    200: 'var(--primary-200)',
+                    300: 'var(--primary-300)',
+                    400: 'var(--primary-400)',
+                    500: 'var(--primary-500)',
+                    600: 'var(--primary-600)',
+                    700: 'var(--primary-700)',
+                    800: 'var(--primary-800)',
+                    900: 'var(--primary-900)'
                 },
                 'primary-dark': {
                     50: 'var(--primary-dark-50)',
@@ -39,6 +42,30 @@ export default {
                     700: 'var(--secondary-700)',
                     800: 'var(--secondary-800)',
                     900: 'var(--secondary-900)'
+                },
+                'accent-1': {
+                    50: 'var(--accent-1-50)',
+                    100: 'var(--accent-1-100)',
+                    200: 'var(--accent-1-200)',
+                    300: 'var(--accent-1-300)',
+                    400: 'var(--accent-1-400)',
+                    500: 'var(--accent-1-500)',
+                    600: 'var(--accent-1-600)',
+                    700: 'var(--accent-1-700)',
+                    800: 'var(--accent-1-800)',
+                    900: 'var(--accent-1-900)'
+                },
+                'accent-2': {
+                    50: 'var(--accent-2-50)',
+                    100: 'var(--accent-2-100)',
+                    200: 'var(--accent-2-200)',
+                    300: 'var(--accent-2-300)',
+                    400: 'var(--accent-2-400)',
+                    500: 'var(--accent-2-500)',
+                    600: 'var(--accent-2-600)',
+                    700: 'var(--accent-2-700)',
+                    800: 'var(--accent-2-800)',
+                    900: 'var(--accent-2-900)'
                 }
             }
         }
